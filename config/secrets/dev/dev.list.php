5fc1858e8faf14553893ef3aed5d7a2d6465cd4a
Valentin MARCEAU <marceauvalentin@gmail.com>
2025-04-17 11:54:45 +0200

    add jwt token authentication

--- config/secrets/dev/dev.list.php
+++ config/secrets/dev/dev.list.php
@@ -1,8 +1,12 @@
 <?php
 
 return [
+    'ADMIN_PASSWORD' => null,
     'DATABASE_NAME' => null,
     'DATABASE_PASSWORD' => null,
     'DATABASE_SERVER' => null,
     'DATABASE_USER' => null,
+    'DM_PASSWORD' => null,
+    'JWT_PHRASE' => null,
+    'PLAYER_PASSWORD' => null,
 ];
