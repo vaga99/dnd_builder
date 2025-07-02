d5739341235361915ea077cbafef904d530dc14c
Valentin MARCEAU <marceauvalentin@gmail.com>
2025-07-02 19:09:15 +0200

    finish basic crud

--- src/Controller/CharacterController.php
+++ src/Controller/CharacterController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Character;
 use App\Entity\Classe;
+use App\Form\Type\CharacterType\CharacterType;
 use App\Repository\CharacterRepository;
 use App\Repository\ClasseRepository;
 use JMS\Serializer\SerializationContext;
@@ -16,13 +17,14 @@ use JMS\Serializer\SerializerInterface;
 use Symfony\Contracts\Cache\ItemInterface;
 use Symfony\Contracts\Cache\TagAwareCacheInterface;
 use App\Form\Type\ClasseType\ClasseType;
+use App\Repository\CharacterClasseRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 final class CharacterController extends AbstractController
 {
     /**
-     * Return all Classe in json format
+     * Return all character
      */
     #[Route('characters', name: 'getCharacters')]
     public function getAllCharacters(
@@ -50,14 +52,72 @@ final class CharacterController extends AbstractController
      * Return a Character in front
      */
     #[Route('characters/{id}', name: 'getCharacter')]
-    public function getCharacterDetails(
+    public function getCharacter(
         Character $character, 
-        SerializerInterface $serializer, 
         CharacterRepository $characterRepository
     ): Response
     {
         return $this->render('character/details.html.twig', [
             'character' => $character,
         ]);
+    }
+
+    /**
+     * Edit a Character in front
+     */
+    #[Route('characters/edit/{id}', name: 'editCharacter')]
+    public function editCharacter(
+        Character $character, 
+        Request $request, 
+        EntityManagerInterface $em,
+        TagAwareCacheInterface $cache
+    ): Response
+    {
+        $form = $this->createForm(CharacterType::class, $character);
+        
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // $form->getData() holds the submitted values
+            // but, the original `$task` variable has also been updated
+            $character = $form->getData();
+
+            $em->persist($character);
+            $em->flush();
+
+            return $this->redirectToRoute('getCharacter', ['id' => $character->getId()]);
+        }
+
+        $cache->invalidateTags(["charactersCache"]);
+
+        return $this->render('character/edit.html.twig', [
+            'character' => $character,
+            'form' => $form
+        ]);
+    }
+
+    /**
+     * Delete Character
+     */
+    #[Route('characters/delete/{id}', name: 'deleteCharacter')]
+    public function deleteCharacter(
+        Character $character, 
+        EntityManagerInterface $em,
+        CharacterClasseRepository $charaClasserepo,
+        TagAwareCacheInterface $cache
+    ): Response
+    {
+
+        $characterClasses = $charaClasserepo->findByCharacter($character);
+
+        foreach ($characterClasses as $key => $characterClasse) {
+            $em->remove($characterClasse);
+        }
+
+        $em->remove($character);
+        $em->flush();
+        $cache->invalidateTags(["charactersCache"]);
+
+        return $this->redirectToRoute('getCharacters');
     }
 }
